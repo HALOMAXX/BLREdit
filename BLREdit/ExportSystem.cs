4eab4a19338fe1cba214ede0b9f8042e8d48c532
HKN1 <halomaxxsteam@googlemail.com>
2022-01-15 16:16:57 +0100

    Changed .Net Runtime from 6 to 4.8 for better compatibility.

--- BLREdit/ExportSystem.cs
+++ BLREdit/ExportSystem.cs
@@ -18,7 +18,7 @@ namespace BLREdit
 
         public static ObservableCollection<Profile> LoadAllProfiles()
         {
-            ObservableCollection<Profile> profiles = new();
+            ObservableCollection<Profile> profiles = new ObservableCollection<Profile>();
             Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + IOResources.PROFILE_DIR);
             foreach (string file in Directory.EnumerateFiles(AppDomain.CurrentDomain.BaseDirectory + IOResources.PROFILE_DIR))
             {
@@ -37,7 +37,7 @@ namespace BLREdit
         {
             try
             {
-                System.Windows.Clipboard.SetText(JsonSerializer.Serialize<Profile>(profile, IOResources.JSO));
+                System.Windows.Clipboard.SetText("register \n" + JsonSerializer.Serialize<Profile>(profile, IOResources.JSO));
             }
             catch
             {
@@ -54,7 +54,7 @@ namespace BLREdit
         {
             foreach (Profile profile in Profiles)
             {
-                StreamWriter sw = new(File.Create(AppDomain.CurrentDomain.BaseDirectory+IOResources.PROFILE_DIR+profile.PlayerName+".json"));
+                StreamWriter sw = new StreamWriter(File.Create(AppDomain.CurrentDomain.BaseDirectory+IOResources.PROFILE_DIR+profile.PlayerName+".json"));
                 sw.Write(JsonSerializer.Serialize<Profile>(profile, IOResources.JSO));
                 sw.Close();
             }
@@ -111,7 +111,7 @@ namespace BLREdit
 
     public class Weapon
     {
-        public string Reciever { get; set; } = "Assault Rifle";
+        public string Receiver { get; set; } = "Assault Rifle";
         public int Muzzle { get; set; } = 8;
         public string Stock { get; set; } = "Silverwood Standard Stock";
         public string Barrel { get; set; } = "Frontier Standard Barrel";
@@ -124,14 +124,14 @@ namespace BLREdit
             ImportItem item = null;
             foreach (ImportItem primary in ImportSystem.Weapons.primary)
             {
-                if (primary.name == Reciever)
+                if (primary.name == Receiver)
                 { 
                     item = primary;
                 }
             }
             foreach (ImportItem secondary in ImportSystem.Weapons.secondary)
             {
-                if (secondary.name == Reciever)
+                if (secondary.name == Receiver)
                 {
                     item = secondary;
                 }
@@ -206,34 +206,34 @@ namespace BLREdit
 
         public static readonly Weapon[] DefaultWeapons = new Weapon[]
         {
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 115,  Muzzle = 8,     Reciever = "Heavy Assault Rifle",       Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 0  Heavy Assault Rifle
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 104,  Muzzle = 8,     Reciever = "LMG Recon",                 Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 1  LMG Recon
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 107,  Muzzle = 8,     Reciever = "Tactical SMG",              Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 2  Tactical SMG
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 140,  Muzzle = 8,     Reciever = "Burstfire SMG",             Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 3  Burstfire SMG
-            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 0,    Muzzle = 0,     Reciever = "Anti-Materiel Rifle",       Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 4  Anti-Materiel Rifle
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 149,  Muzzle = 8,     Reciever = "Bullpup Full Auto",         Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 5  Bullpup Full Auto
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 195,  Muzzle = 8,     Reciever = "AK470 Rifle",               Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 6  AK470 Rifle
-            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 205,  Muzzle = 0,     Reciever = "Compound Bow",              Stock = "No Stock",                             Scope="No Optic Mod" },                 // 7  Compound Bow
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 185,  Muzzle = 8,     Reciever = "M4X Rifle",                 Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 8  M4X Rifle
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 212,  Muzzle = 8,     Reciever = "Tactical Assault Rifle",    Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 9  Tactical Assault Rifle
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 9,    Muzzle = 8,     Reciever = "Assault Rifle",             Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 10 Assault Rifle
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 90,   Muzzle = 8,     Reciever = "Bolt-Action Rifle",         Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 11 Bolt-Action Rifle
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 39,   Muzzle = 8,     Reciever = "Light Machine Gun",         Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 12 Light Machine Gun
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 81,   Muzzle = 8,     Reciever = "Burstfire Rifle",           Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 13 Burstfire Rifle
-            new Weapon() { Barrel = "Prestige Frontier Standard Barrel",    Grip = "", Magazine = 226,  Muzzle = 17,    Reciever = "Prestige Assault Rifle",    Stock = "Prestige Silverwood Standard Stock",   Scope="Prestige Titan Rail Sight" },    // 14 Prestige Assault Rifle
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 0,    Muzzle = 8,     Reciever = "Submachine Gun",            Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 15 Submachine Gun
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 20,   Muzzle = 8,     Reciever = "Combat Rifle",              Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 16 Combat Rifle
-            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 221,  Muzzle = 8,     Reciever = "Light Recon Rifle",         Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 17 Light Recon Rifle
-            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 124,  Muzzle = -1,     Reciever = "Shotgun AR-k",              Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 18 Shotgun AR-k
-            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 134,  Muzzle = -1,     Reciever = "Breech Loaded Pistol",      Stock = "No Stock",                             Scope="No Optic Mod" },                 // 19 Breech Loaded Pistol
-            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 177,  Muzzle = -1,     Reciever = "Snub 260",                  Stock = "No Stock",                             Scope="No Optic Mod" },                 // 20 Snub 260
-            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 45,   Muzzle = -1,     Reciever = "Heavy Pistol",              Stock = "No Stock",                             Scope="No Optic Mod" },                 // 21 Heavy Pistol
-            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 54,   Muzzle = -1,     Reciever = "Light Pistol",              Stock = "No Stock",                             Scope="No Optic Mod" },                 // 22 Light Pistol
-            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 72,   Muzzle = -1,     Reciever = "Burstfire Pistol",          Stock = "No Stock",                             Scope="No Optic Mod" },                 // 23 Burstfire Pistol
-            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 227,  Muzzle = -1,     Reciever = "Prestige Light Pistol",     Stock = "No Stock",                             Scope="No Optic Mod" },                 // 24 Prestige Light Pistol
-            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 63,   Muzzle = -1,     Reciever = "Machine Pistol",            Stock = "No Stock",                             Scope="No Optic Mod" },                 // 25 Machine Pistol
-            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 99,   Muzzle = -1,     Reciever = "Revolver",                  Stock = "No Stock",                             Scope="No Optic Mod" },                 // 26 Revolver
-            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 29,   Muzzle = -1,     Reciever = "Shotgun",                   Stock = "Silverwood Standard Stock",            Scope="No Optic Mod" },                 // 27 Shotgun
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 115,  Muzzle = 8,     Receiver = "Heavy Assault Rifle",       Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 0  Heavy Assault Rifle
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 104,  Muzzle = 8,     Receiver = "LMG Recon",                 Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 1  LMG Recon
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 107,  Muzzle = 8,     Receiver = "Tactical SMG",              Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 2  Tactical SMG
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 140,  Muzzle = 8,     Receiver = "Burstfire SMG",             Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 3  Burstfire SMG
+            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 0,    Muzzle = 0,     Receiver = "Anti-Materiel Rifle",       Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 4  Anti-Materiel Rifle
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 149,  Muzzle = 8,     Receiver = "Bullpup Full Auto",         Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 5  Bullpup Full Auto
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 195,  Muzzle = 8,     Receiver = "AK470 Rifle",               Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 6  AK470 Rifle
+            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 205,  Muzzle = 0,     Receiver = "Compound Bow",              Stock = "No Stock",                             Scope="No Optic Mod" },                 // 7  Compound Bow
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 185,  Muzzle = 8,     Receiver = "M4X Rifle",                 Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 8  M4X Rifle
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 212,  Muzzle = 8,     Receiver = "Tactical Assault Rifle",    Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 9  Tactical Assault Rifle
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 9,    Muzzle = 8,     Receiver = "Assault Rifle",             Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 10 Assault Rifle
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 90,   Muzzle = 8,     Receiver = "Bolt-Action Rifle",         Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 11 Bolt-Action Rifle
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 39,   Muzzle = 8,     Receiver = "Light Machine Gun",         Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 12 Light Machine Gun
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 81,   Muzzle = 8,     Receiver = "Burstfire Rifle",           Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 13 Burstfire Rifle
+            new Weapon() { Barrel = "Prestige Frontier Standard Barrel",    Grip = "", Magazine = 226,  Muzzle = 17,    Receiver = "Prestige Assault Rifle",    Stock = "Prestige Silverwood Standard Stock",   Scope="Prestige Titan Rail Sight" },    // 14 Prestige Assault Rifle
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 0,    Muzzle = 8,     Receiver = "Submachine Gun",            Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 15 Submachine Gun
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 20,   Muzzle = 8,     Receiver = "Combat Rifle",              Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 16 Combat Rifle
+            new Weapon() { Barrel = "Frontier Standard Barrel",             Grip = "", Magazine = 221,  Muzzle = 8,     Receiver = "Light Recon Rifle",         Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 17 Light Recon Rifle
+            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 124,  Muzzle = -1,     Receiver = "Shotgun AR-k",              Stock = "Silverwood Standard Stock",            Scope="Titan Rail Sight" },             // 18 Shotgun AR-k
+            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 134,  Muzzle = -1,     Receiver = "Breech Loaded Pistol",      Stock = "No Stock",                             Scope="No Optic Mod" },                 // 19 Breech Loaded Pistol
+            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 177,  Muzzle = -1,     Receiver = "Snub 260",                  Stock = "No Stock",                             Scope="No Optic Mod" },                 // 20 Snub 260
+            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 45,   Muzzle = -1,     Receiver = "Heavy Pistol",              Stock = "No Stock",                             Scope="No Optic Mod" },                 // 21 Heavy Pistol
+            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 54,   Muzzle = -1,     Receiver = "Light Pistol",              Stock = "No Stock",                             Scope="No Optic Mod" },                 // 22 Light Pistol
+            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 72,   Muzzle = -1,     Receiver = "Burstfire Pistol",          Stock = "No Stock",                             Scope="No Optic Mod" },                 // 23 Burstfire Pistol
+            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 227,  Muzzle = -1,     Receiver = "Prestige Light Pistol",     Stock = "No Stock",                             Scope="No Optic Mod" },                 // 24 Prestige Light Pistol
+            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 63,   Muzzle = -1,     Receiver = "Machine Pistol",            Stock = "No Stock",                             Scope="No Optic Mod" },                 // 25 Machine Pistol
+            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 99,   Muzzle = -1,     Receiver = "Revolver",                  Stock = "No Stock",                             Scope="No Optic Mod" },                 // 26 Revolver
+            new Weapon() { Barrel = "No Barrel Mod",                        Grip = "", Magazine = 29,   Muzzle = -1,     Receiver = "Shotgun",                   Stock = "Silverwood Standard Stock",            Scope="No Optic Mod" },                 // 27 Shotgun
         };
 
         public static Weapon DefaultAssaultRifle { get { return DefaultWeapons[10]; } }
