194fbdaad0cf519c91c107bb19bb72768da5f25e
HKN1 <halomaxxsteam@googlemail.com>
2022-01-27 00:37:49 +0100

    changed default gender to male

--- BLREdit/API/ExportSystem/Profile.cs
+++ BLREdit/API/ExportSystem/Profile.cs
@@ -8,7 +8,7 @@ namespace BLREdit
         public Loadout Loadout1 { get; set; } = Loadout.DefaultLoadout1;
         public Loadout Loadout2 { get; set; } = Loadout.DefaultLoadout2;
         public Loadout Loadout3 { get; set; } = Loadout.DefaultLoadout3;
-        public bool IsFemale { get; set; } = true;
+        public bool IsFemale { get; set; } = false;
         public string Helmet { get; set; } = "Overmatch Armet X";
         public string UpperBody { get; set; } = "Overmatch Anaconda KX7";
         public string LowerBody { get; set; } = "Allied LZ Viper";
