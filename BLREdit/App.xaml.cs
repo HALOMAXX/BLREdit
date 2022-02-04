8d8834cc2049e8b638f23f695b21fb648749acfb
HKN1 <halomaxxsteam@googlemail.com>
2022-02-04 21:40:36 +0100

    version change to reflect additions

--- BLREdit/App.xaml.cs
+++ BLREdit/App.xaml.cs
@@ -22,8 +22,8 @@ namespace BLREdit
             VersionCheck();
             ImportSystem.Initialize();
         }
-        const string CurrentVersion = "v0.3.3";
-        const string CurrentVersionName = "BLREdit Hotfix for disappearing stocks on secondary weapons";
+        const string CurrentVersion = "v0.3.4";
+        const string CurrentVersionName = "BLREdit Depot Items as Secondaries";
         public const string CurrentOwner = "HALOMAXX";
         public const string CurrentRepo = "BLREdit";
         public static void VersionCheck()
