cbf194756f65a1ff18a18f4cdba9e71d2577bd97
HKN1 <halomaxxsteam@googlemail.com>
2022-07-14 03:38:31 +0200

    fixed copy error in stat update missed some primary/secondary

--- BLREdit/UI/MainWindow.xaml.cs
+++ BLREdit/UI/MainWindow.xaml.cs
@@ -191,8 +191,8 @@ namespace BLREdit.UI
             PrimaryRecoilLabel.Content = Loadout.Primary.RecoilHip.ToString("0.00") + '°';
             PrimaryZoomRecoilLabel.Content = Loadout.Primary.RecoilZoom.ToString("0.00") + '°';
             PrimaryZoomLabel.Content = Loadout.Primary.ZoomMagnification.ToString("0.0");
-            PrimaryScopeInLabel.Content = Loadout.Primary.ModifiedScopeInTime.ToString("0.00") + 's';
-            PrimaryRangeLabel.Content = Loadout.Primary.RangeClose.ToString("0") + '/' + Loadout.Primary.RangeFar.ToString("0") + '/' + Loadout.Primary.RangeTracer.ToString("0");
+            PrimaryScopeInLabel.Content = Loadout.Primary.ModifiedScopeInTime.ToString("0.000") + 's';
+            PrimaryRangeLabel.Content = Loadout.Primary.RangeClose.ToString("0.0") + '/' + Loadout.Primary.RangeFar.ToString("0.0") + '/' + Loadout.Primary.RangeTracer.ToString("0");
             PrimaryRunLabel.Content = Loadout.Primary.ModifiedRunSpeed.ToString("0.00");
             PrimaryDescriptorLabel.Content = Loadout.Primary.WeaponDescriptor;
 
@@ -215,9 +215,9 @@ namespace BLREdit.UI
             #endregion PrimaryStats
 
             #region SecondaryStats
-            SecondaryDamageLabel.Content = Loadout.Secondary.DamageClose.ToString("0") + '/' + Loadout.Primary.DamageFar.ToString("0");
+            SecondaryDamageLabel.Content = Loadout.Secondary.DamageClose.ToString("0") + '/' + Loadout.Secondary.DamageFar.ToString("0");
             SecondaryRateOfFireLabel.Content = Loadout.Secondary.ModifiedRateOfFire.ToString("0");
-            SecondaryAmmoLabel.Content = Loadout.Secondary.ModifiedAmmoMagazine.ToString("0") + '/' + Loadout.Primary.ModifiedAmmoReserve.ToString("0");
+            SecondaryAmmoLabel.Content = Loadout.Secondary.ModifiedAmmoMagazine.ToString("0") + '/' + Loadout.Secondary.ModifiedAmmoReserve.ToString("0");
             SecondaryReloadLabel.Content = Loadout.Secondary.ModifiedReloadSpeed.ToString("0.00") + 's';
             SecondarySwapLabel.Content = Loadout.Secondary.RawSwapRate.ToString("0");
             SecondaryAimLabel.Content = Loadout.Secondary.SpreadWhileADS.ToString("0.00") + '°';
@@ -226,20 +226,20 @@ namespace BLREdit.UI
             SecondaryRecoilLabel.Content = Loadout.Secondary.RecoilHip.ToString("0.00") + '°';
             SecondaryZoomRecoilLabel.Content = Loadout.Secondary.RecoilZoom.ToString("0.00") + '°';
             SecondaryZoomLabel.Content = Loadout.Secondary.ZoomMagnification.ToString("0.0");
-            SecondaryScopeInLabel.Content = Loadout.Secondary.ModifiedScopeInTime.ToString("0.00") + 's';
-            SecondaryRangeLabel.Content = Loadout.Secondary.RangeClose.ToString("0") + '/' + Loadout.Primary.RangeFar.ToString("0") + '/' + Loadout.Primary.RangeTracer.ToString("0");
+            SecondaryScopeInLabel.Content = Loadout.Secondary.ModifiedScopeInTime.ToString("0.000") + 's';
+            SecondaryRangeLabel.Content = Loadout.Secondary.RangeClose.ToString("0.0") + '/' + Loadout.Secondary.RangeFar.ToString("0.0") + '/' + Loadout.Secondary.RangeTracer.ToString("0");
             SecondaryRunLabel.Content = Loadout.Secondary.ModifiedRunSpeed.ToString("0.00");
             SecondaryDescriptorLabel.Content = Loadout.Secondary.WeaponDescriptor;
 
-            SecondaryRecoilVerticalRatioLabel.Content = Loadout.Primary.VerticalRecoilRatio.ToString("0.00");
-            SecondarySpreadCenterWeightLabel.Content = Loadout.Primary.SpreadCenterWeight.ToString("0.00");
-            SecondarySpreadCenterLabel.Content = Loadout.Primary.SpreadCenter.ToString("0.00");
-            SecondaryFragmentsPerShellLabel.Content = Loadout.Primary.FragmentsPerShell.ToString("0");
-            SecondaryZoomFirerateLabel.Content = Loadout.Primary.ZoomRateOfFire.ToString("0");
-            SecondarySpreadCrouchMultiplierLabel.Content = Loadout.Primary.SpreadCrouchMultiplier.ToString("0.00");
-            SecondarySpreadJumpMultiplierLabel.Content = Loadout.Primary.SpreadJumpMultiplier.ToString("0.00");
-            SecondaryRecoilRecoveryTimeLabel.Content = Loadout.Primary.RecoilRecoveryTime.ToString("0.00");
-            SecondaryModReloadLabel.Content = Loadout.Primary.ModifiedReloadSpeed.ToString("0.00") + 's';
+            SecondaryRecoilVerticalRatioLabel.Content = Loadout.Secondary.VerticalRecoilRatio.ToString("0.00");
+            SecondarySpreadCenterWeightLabel.Content = Loadout.Secondary.SpreadCenterWeight.ToString("0.00");
+            SecondarySpreadCenterLabel.Content = Loadout.Secondary.SpreadCenter.ToString("0.00");
+            SecondaryFragmentsPerShellLabel.Content = Loadout.Secondary.FragmentsPerShell.ToString("0");
+            SecondaryZoomFirerateLabel.Content = Loadout.Secondary.ZoomRateOfFire.ToString("0");
+            SecondarySpreadCrouchMultiplierLabel.Content = Loadout.Secondary.SpreadCrouchMultiplier.ToString("0.00");
+            SecondarySpreadJumpMultiplierLabel.Content = Loadout.Secondary.SpreadJumpMultiplier.ToString("0.00");
+            SecondaryRecoilRecoveryTimeLabel.Content = Loadout.Secondary.RecoilRecoveryTime.ToString("0.00");
+            SecondaryModReloadLabel.Content = Loadout.Secondary.ModifiedReloadSpeed.ToString("0.00") + 's';
 
             SecondaryModAccuracyLabel.Content = Loadout.Secondary.AccuracyPercentage.ToString("0") + '%';
             SecondaryModDamageLabel.Content = Loadout.Secondary.DamagePercentage.ToString("0") + '%';
