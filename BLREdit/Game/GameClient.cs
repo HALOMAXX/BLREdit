fe33d9cf1251c2a548c2d15ee6617427b8274160
HKN1 <halomaxxsteam@googlemail.com>
2022-07-29 19:56:01 +0200

    added client version check

--- BLREdit/Game/GameClient.cs
+++ BLREdit/Game/GameClient.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -20,23 +21,49 @@ public class GameClient : INotifyPropertyChanged
     [JsonIgnore] public bool IsNotPatched { get { return !IsPatched; } private set { OnPropertyChanged(); } }
     [JsonIgnore] public bool IsCurrentClient { get { return PatchedPath == BLREditSettings.Settings?.DefaultClient?.PatchedPath && IsPatched; } set { IsNotCurrentClient = false; OnPropertyChanged(); } }
     [JsonIgnore] public bool IsNotCurrentClient { get { return !IsCurrentClient; } private set { OnPropertyChanged(); } }
-    private string originalPath;
-    public string OriginalPath { get { return originalPath; } set { if (originalPath != value && !string.IsNullOrEmpty(value) && File.Exists(value)) { originalPath = value; IsPatched = false; OnPropertyChanged(); } } }
-    private string patchedPath;
+
+    [JsonIgnore] private string clientHash;
+    public string ClientHash { get { return clientHash; } 
+        set 
+        { 
+            if (clientHash != value && !string.IsNullOrEmpty(value)) 
+            { 
+                clientHash = value;
+                VersionHashes.TryGetValue(ClientHash, out int version);
+                ClientVersion = version;
+                OnPropertyChanged(); 
+            }
+        }
+    }
+
+    [JsonIgnore] private int version;
+    public int ClientVersion { get { return version; } set { version = value; OnPropertyChanged(); } }
+
+    [JsonIgnore] private string originalPath;
+    public string OriginalPath { get { return originalPath; } set { if (originalPath != value && !string.IsNullOrEmpty(value) && File.Exists(value)) { originalPath = value; IsPatched = false; ClientHash = CreateClientHash(value);  OnPropertyChanged(); } } }
+    
+    [JsonIgnore] private string patchedPath;
     public string PatchedPath { get { return patchedPath; } set { if (patchedPath != value && !string.IsNullOrEmpty(value) && File.Exists(value)) { patchedPath = value; IsPatched = false; OnPropertyChanged(); } } }
 
-    
-    private bool patchEmblem = true;
+    [JsonIgnore] private bool patchEmblem = true;
     public bool PatchEmblem { get { return patchEmblem; } set { patchEmblem = value; OnPropertyChanged(); } }
-    
-    private bool patchProxy = false;
+
+    [JsonIgnore] private bool patchProxy = false;
     public bool PatchProxy { get { return patchProxy; } set { patchProxy = value; OnPropertyChanged(); } }
 
-    private bool isProxy = false;
+    [JsonIgnore] private bool isProxy = false;
     public bool IsProxyPatched { get { return isProxy; } private set { isProxy = value; OnPropertyChanged(); } }
-    
-    private bool isEmblem = false;
+
+    [JsonIgnore] private bool isEmblem = false;
     public bool IsEmblemPatched { get { return isEmblem; } private set { isEmblem = value; OnPropertyChanged(); } }
+
+    public static Dictionary<string, int> VersionHashes => new()
+    {
+        {"0f4a732484f566d928c580afdae6ef01c002198dd7158cb6de29b9a4960064c7", 302},
+        {"de08147e419ed89d6db050b4c23fa772338132587f6b533b6233733f9bce46c3", 301},
+        {"1742df917761f9dc01b079ae2aad78ef2ff17562af1dad6ad6ea7cf3622fe7f6", 300},
+        {"d4f9cec736a83f7930f04438344d35ff9f0e57212755974bd51f48ff89d303c4", 120}
+    };
 
     public event PropertyChangedEventHandler PropertyChanged;
     private void OnPropertyChanged([CallerMemberName] string propertyName = null)
@@ -73,6 +100,17 @@ public class GameClient : INotifyPropertyChanged
                 );
             }
             return launchClientCommand;
+        }
+    }
+
+    private static string CreateClientHash(string filePath)
+    {
+        using (var crypto = SHA256.Create())
+        {
+            using (var stream = File.OpenRead(filePath))
+            { 
+                return BitConverter.ToString(crypto.ComputeHash(stream)).Replace("-", string.Empty).ToLower();
+            }
         }
     }
 
