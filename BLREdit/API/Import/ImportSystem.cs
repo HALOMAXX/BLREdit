5bb5f921f8d758d5e67512eefd2fa86f20cc25c1
MagicalChicken <eliteultra12@gmail.com>
2022-10-03 02:19:37 -0400

    more appealing and easier to read positive/negative stat colors

--- BLREdit/API/Import/ImportSystem.cs
+++ BLREdit/API/Import/ImportSystem.cs
@@ -472,10 +472,10 @@ public static class ImportSystem
 
     static readonly Brush grey = new SolidColorBrush(Color.FromArgb(136, 136, 136, 136));
 
-    static readonly Brush defaultGreen = new SolidColorBrush(Color.FromArgb(255,0,136,0));
+    static readonly Brush defaultGreen = new SolidColorBrush(Color.FromArgb(255,110,175,125));
     static readonly Brush highlightGreen = new SolidColorBrush(Color.FromArgb(255, 0, 255, 0));
 
-    static readonly Brush defaultRed = new SolidColorBrush(Color.FromArgb(255, 136, 0, 0));
+    static readonly Brush defaultRed = new SolidColorBrush(Color.FromArgb(255, 200, 60, 50));
     static readonly Brush highlightRed = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
     private static void FormatDisplayStat(ref DisplayStatDiscriptor desc, string propertyName, string description, object value, StatsEnum type, string format, string suffix = "", string prefix = "", int count = -1)
     { 
