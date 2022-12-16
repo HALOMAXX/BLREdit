3f9668d3629cb97f30bb41fa24c8bc19b6de7b64
HALOMAXX <halomaxxsteam@googlemail.com>
2022-12-16 15:20:50 +0100

    Changed Behaviour of GearSlot Export to Respect in Game Limits. #28

--- BLREdit/API/Export/SEGear.cs
+++ BLREdit/API/Export/SEGear.cs
@@ -31,18 +31,18 @@ public sealed class SEGear
     public SEGear(BLRLoadout loadout)
     {
         Female = loadout.IsFemale;
-        BodyCamo = ImportSystem.GetIDOfItem(loadout.BodyCamo); ;// loadout?.BodyCamo?.LMID ?? 0;
-        UpperBody = ImportSystem.GetIDOfItem(loadout.UpperBody); ;// loadout?.UpperBody?.LMID ?? 0;
-        LowerBody = ImportSystem.GetIDOfItem(loadout.LowerBody); ;// loadout?.LowerBody?.LMID ?? 0;
-        Helmet = ImportSystem.GetIDOfItem(loadout.Helmet); ;// loadout?.Helmet?.LMID ?? 0;
-        Tactical = ImportSystem.GetIDOfItem(loadout.Tactical); ;// loadout?.Tactical?.LMID ?? 0;
+        BodyCamo = ImportSystem.GetIDOfItem(loadout.BodyCamo);
+        UpperBody = ImportSystem.GetIDOfItem(loadout.UpperBody);
+        LowerBody = ImportSystem.GetIDOfItem(loadout.LowerBody);
+        Helmet = ImportSystem.GetIDOfItem(loadout.Helmet);
+        Tactical = ImportSystem.GetIDOfItem(loadout.Tactical);
         int avatar = ImportSystem.GetIDOfItem(loadout.Avatar);
         if (avatar > 34) { Avatar = -1; } else { Avatar = avatar; }
-        Badge = ImportSystem.GetIDOfItem(loadout.Trophy); ;// loadout?.Trophy?.LMID ?? 0;
-        Gear_R1 = ImportSystem.GetIDOfItem(loadout.Gear1); ;// loadout?.Gear1?.LMID ?? 0;
-        Gear_R2 = ImportSystem.GetIDOfItem(loadout.Gear2); ;// loadout?.Gear2?.LMID ?? 1;
-        Gear_L1 = ImportSystem.GetIDOfItem(loadout.Gear3); ;// loadout?.Gear3?.LMID ?? 2;
-        Gear_L2 = ImportSystem.GetIDOfItem(loadout.Gear4); ;// loadout?.Gear4?.LMID ?? 3;
+        Badge = ImportSystem.GetIDOfItem(loadout.Trophy);
+        if(loadout.GearSlot1Bool.Is) Gear_R1 = ImportSystem.GetIDOfItem(loadout.Gear1);
+        if (loadout.GearSlot2Bool.Is) Gear_R2 = ImportSystem.GetIDOfItem(loadout.Gear2);
+        if (loadout.GearSlot3Bool.Is) Gear_L1 = ImportSystem.GetIDOfItem(loadout.Gear3);
+        if (loadout.GearSlot4Bool.Is) Gear_L2 = ImportSystem.GetIDOfItem(loadout.Gear4);
 
         //TODO Hanger, Icon, IconColor, PatchShape, PatchColor, ButtPack   
     }
